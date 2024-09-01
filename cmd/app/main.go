b39dc74db8c80a74a17772d669cb67d2eab95cec
BrandtSwanson <brandtswan@gmail.com>
2024-09-01 17:48:19 -0500

    INIT PERSONAL

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -42,8 +42,20 @@ type Event struct {
 	Date    time.Time `json:"date"`
 }
 
+type Personal struct {
+	TimeStamp time.Time `json:"timestamp"`
+	ID      int       `json:"id"`
+	Water      int       `json:"water"`
+	Joyousness int `json:"joyousness`
+	Relaxation int `json:"relaxation`
+	Alertness int `json:"alertness`
+	ScreenTime int `json:"screenTime`
+	Satisfaction int `json:"satisfaction`
+	OutsideTime int `json:"outsideTime`
+};
+
 func main() {
-	connectionString := "mongodb://ip:port/database"
+	connectionString := "mongodb://192.168.4.39:27017/database"
 
 	// // Set client options
 	clientOptions := options.Client().ApplyURI(connectionString)
@@ -77,6 +89,8 @@ func main() {
 	}()
 
 	fmt.Println("Connected to MongoDB!")
+	
+	//Check for database collections, instantiate ones that don't exist
 
 	// Now you can perform database operations using the 'client'
 
@@ -89,14 +103,15 @@ func main() {
 	// Create a new Chi router
 	r := chi.NewRouter()
 	var notes []Note
-	var goals []Goal
 	var events []Event
+	var personal []Personal
 
 	// Apply your custom CORS middleware to the router
 	r.Use(middleware.NewCORS().Handler) // Use your custom CORS middleware
 
 	// Retrieve all notes
 	r.Get("/api/notes", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("ayo----\n\nBo\n-----")
 		// Return all notes as JSON
 		coll := client.Database("mindmesh").Collection("notes")
 		filter := bson.D{}
@@ -182,82 +197,6 @@ func main() {
 		w.Write([]byte("Note deleted successfully"))
 	})
 
-	// Create a new goal
-	r.Post("/api/goals", func(w http.ResponseWriter, r *http.Request) {
-		var newGoal Goal
-		err := json.NewDecoder(r.Body).Decode(&newGoal)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		// Assign a unique ID and add the goal to the slice
-		goals = append(goals, newGoal)
-
-		// Return the created goal as JSON
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(newGoal)
-	})
-
-	// Retrieve all goals
-	r.Get("/api/goals", func(w http.ResponseWriter, r *http.Request) {
-		// Return all goals as JSON
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(goals)
-	})
-
-	// Retrieve a single goal by ID
-	r.Get("/api/goals/{goalID}", func(w http.ResponseWriter, r *http.Request) {
-		goalID := chi.URLParam(r, "goalID")
-		for _, goal := range goals {
-			if strconv.Itoa(goal.ID) == goalID {
-				// Return the goal as JSON
-				w.Header().Set("Content-Type", "application/json")
-				json.NewEncoder(w).Encode(goal)
-				return
-			}
-		}
-		// If goal is not found, return a 404
-		http.NotFound(w, r)
-	})
-
-	// Update a goal by ID
-	r.Put("/api/goals/{goalID}", func(w http.ResponseWriter, r *http.Request) {
-		goalID := chi.URLParam(r, "goalID")
-		var updatedGoal Goal
-		err := json.NewDecoder(r.Body).Decode(&updatedGoal)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		for i, goal := range goals {
-			if strconv.Itoa(goal.ID) == goalID {
-				// Update the goal
-				goals[i] = updatedGoal
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-		}
-		// If goal is not found, return a 404
-		http.NotFound(w, r)
-	})
-
-	// Delete a goal by ID
-	r.Delete("/api/goals/{goalID}", func(w http.ResponseWriter, r *http.Request) {
-		goalID := chi.URLParam(r, "goalID")
-		for i, goal := range goals {
-			if strconv.Itoa(goal.ID) == goalID {
-				// Remove the goal from the slice
-				goals = append(goals[:i], goals[i+1:]...)
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-		}
-		// If goal is not found, return a 404
-		http.NotFound(w, r)
-	})
-
 	// Create a new note
 	r.Post("/api/notes", func(w http.ResponseWriter, r *http.Request) {
 
@@ -278,22 +217,53 @@ func main() {
 		coll := client.Database("mindmesh").Collection("notes")
 		result, err := coll.InsertOne(context.TODO(), newNote)
 		fmt.Printf("Inserted document with _id: %v\n", result.InsertedID)
-		// title := "Back to the Future"
-		// var result bson.M
-		// err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).Decode(&result)
-		// if err == mongo.ErrNoDocuments {
-		// 	fmt.Printf("No document was found with the title %s\n", title)
-		// 	return
-		// }
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// jsonData, err := json.MarshalIndent(result, "", "    ")
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// fmt.Printf("%s\n", jsonData)
+	})
 
+	// Create a new personal entry
+	r.Post("/api/personal", func(w http.ResponseWriter, r *http.Request) {
+
+		var newPersonalData Personal
+		err := json.NewDecoder(r.Body).Decode(&newPersonalData)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		newPersonalData.TimeStamp = time.Now()
+
+		// Assign a unique ID and add the note to the slice
+		personal = append(personal, newPersonalData)
+
+		fmt.Println("HERE")
+		fmt.Printf("%#v", newPersonalData)
+
+		// Return the created note as JSON
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(newPersonalData)
+
+		coll := client.Database("mindmesh").Collection("personal_data_timeseries")
+		result, err := coll.InsertOne(context.TODO(), newPersonalData)
+		fmt.Printf("Inserted document with _id: %v\n", result.InsertedID)
+	})
+
+	// Retrieve all notes
+	r.Get("/api/personal", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("ayo----\n\nBo\n-----")
+		// Return all notes as JSON
+		coll := client.Database("mindmesh").Collection("personal_data_timeseries")
+		filter := bson.D{}
+		// Retrieves documents that match the query filer
+		var results []bson.M
+		cursor, err := coll.Find(context.TODO(), filter)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if err := cursor.All(context.TODO(), &results); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(results)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(results)
 	})
 
 	// Retrieve all events
